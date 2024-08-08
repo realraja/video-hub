6c4890ba2cc87264460475d03f3750763e154cf9
realraja <146420691+realraja@users.noreply.github.com>
2024-08-08 17:17:17 +0530

    Update Videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -58,9 +58,9 @@ const Videos = () => {
               params.videoid
             ].materials[0].created_at
           );
-          // setVideoSrc(data[course].materials.subjects[params.index].categories[params.videoid].materials[0].link);
-          fetchData(data[course].materials.subjects[params.index].categories[params.videoid].materials[0].link).then((data)=>{
-          setVideoSrc(data.q720);
+          setVideoSrc(data[course].materials.subjects[params.index].categories[params.videoid].materials[0].link);
+          // fetchData(data[course].materials.subjects[params.index].categories[params.videoid].materials[0].link).then((data)=>{
+          // setVideoSrc(data.q720);
           });
         }
       }
